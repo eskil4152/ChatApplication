b6afd7c2be9ae48804a80250c2ae5cfd51435015
eskil4152 <eskiltb@hotmail.com>
2024-03-15 19:24:42 +0100

    Encrypt

--- ChatApplicationServerClient/Crypto.cs
+++ ChatApplicationServerClient/Crypto.cs
@@ -1,18 +1,63 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace ChatApplicationServerClient
 {
     public class Crypto
     {
-        public static string Encrypt()
+        public static string Encrypt(string s, string key)
         {
+            if (String.IsNullOrEmpty(s) || String.IsNullOrEmpty(key))
+            {
+                return s;
+            }
 
+            byte[] iv = new byte[16];
+            byte[] array;
 
-            return "";
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using MemoryStream memoryStream = new();
+                using CryptoStream cryptoStream = new((Stream)memoryStream, encryptor, CryptoStreamMode.Write);
+                using (StreamWriter streamWriter = new((Stream)cryptoStream))
+                {
+                    streamWriter.Write(s);
+                }
+
+                array = memoryStream.ToArray();
+            }
+
+            return Convert.ToBase64String(array);
         }
 
-        public static string Decrypt()
+
+        public static string Decrypt(string key, string s)
         {
-            return "";
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(s);
+
+            using Aes aes = Aes.Create();
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = iv;
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            try
+            {
+                using MemoryStream memoryStream = new(buffer);
+                using CryptoStream cryptoStream = new((Stream)memoryStream, decryptor, CryptoStreamMode.Read);
+                using StreamReader streamReader = new((Stream)cryptoStream);
+                return streamReader.ReadToEnd();
+            }
+            catch
+            {
+                return "Banana?";
+            }
         }
     }
 }
--- ChatApplicationServerClient/Program.cs
+++ ChatApplicationServerClient/Program.cs
@@ -4,79 +4,103 @@ using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 
-var hostIp = "192.168.0.135";
-int hostPort = 8081;
-
-Console.WriteLine("Username:");
-string? username = Console.ReadLine();
-
-try
+namespace ChatApplicationServerClient
 {
-    // Establish connection to server
-    TcpClient client = new(hostIp, hostPort); 
-    Console.WriteLine("Client connected");
-
-    NetworkStream stream = client.GetStream();
-
-    Thread readThread = new(() =>
+    public class Program
     {
-        while (true)
+        public static void Main()
         {
-            if (stream.DataAvailable)
+            var hostIp = "192.168.0.135";
+            int hostPort = 8081;
+
+            string key = "abcd906.abcd906.";
+            string key2 = "abcd906.abcd906!";
+
+            Console.WriteLine("Enter a text: ");
+            string? text = Console.ReadLine();
+
+            string encrypted = Crypto.Encrypt(text, key);
+            string encrypted2 = Crypto.Encrypt(text, key2);
+
+            Console.WriteLine("Encrypted with key: " + encrypted);
+            Console.WriteLine("Encrypted with another key: " + encrypted2);
+
+            Console.WriteLine("Decrypted: " + Crypto.Decrypt(key, encrypted));
+            Console.WriteLine("Decrypted wrong key: " + Crypto.Decrypt(key2, encrypted));
+
+            Console.WriteLine("Username:");
+            string? username = Console.ReadLine();
+
+            try
             {
-                byte[] data = new byte[256];
-                int bytes = stream.Read(data, 0, data.Length);
-                string responseData = Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Server response: " + responseData);
+                // Establish connection to server
+                TcpClient client = new(hostIp, hostPort);
+                Console.WriteLine("Client connected");
+
+                NetworkStream stream = client.GetStream();
+
+                Thread readThread = new(() =>
+                {
+                    while (true)
+                    {
+                        if (stream.DataAvailable)
+                        {
+                            byte[] data = new byte[256];
+                            int bytes = stream.Read(data, 0, data.Length);
+                            string responseData = Encoding.ASCII.GetString(data, 0, bytes);
+                            Console.WriteLine("Server response: " + responseData);
+                        }
+                    }
+                });
+                readThread.Start();
+
+                // Thread for writing messages to the server
+                Thread writeThread = new(() =>
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("What to write?");
+                        string? messageInput = Console.ReadLine();
+
+                        HttpMessage message = new()
+                        {
+                            Username = username,
+                            Password = "password",
+                            RoomNumber = 1,
+                            Message = messageInput
+                        };
+
+                        string json = JsonSerializer.Serialize(message);
+                        byte[] byteArray = Encoding.ASCII.GetBytes(json);
+
+                        stream.Write(byteArray, 0, byteArray.Length);
+                    }
+                });
+                writeThread.Start();
+
+                /*while (true)
+                {
+                    while (!stream.DataAvailable);
+
+                    // Receive Response
+                    byte[] data = new byte[256];
+                    StringBuilder responseData = new();
+                    int bytes = stream.Read(data, 0, data.Length);
+                    responseData.Append(Encoding.ASCII.GetString(data, 0, bytes));
+                    Console.WriteLine("Server response: " + responseData.ToString());
+
+                    // Send data
+                    Console.WriteLine("What to write back?");
+                    message = Console.ReadLine() + "";
+                    string json = JsonSerializer.Serialize(messageObject);
+                    byte[] byteArray = Encoding.ASCII.GetBytes(json);
+                    stream.Write(byteArray, 0, byteArray.Length);
+                }*/
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
             }
         }
-    });
-    readThread.Start();
-
-    // Thread for writing messages to the server
-    Thread writeThread = new(() =>
-    {
-        while (true)
-        {
-            Console.WriteLine("What to write?");
-            string? messageInput = Console.ReadLine();
-
-            HttpMessage message = new()
-            {
-                Username = username,
-                Password = "password",
-                RoomNumber = 1,
-                Message = messageInput
-            };
-
-            string json = JsonSerializer.Serialize(message);
-            byte[] byteArray = Encoding.ASCII.GetBytes(json);
-
-            stream.Write(byteArray, 0, byteArray.Length);
-        }
-    });
-    writeThread.Start();
-
-    /*while (true)
-    {
-        while (!stream.DataAvailable);
-
-        // Receive Response
-        byte[] data = new byte[256];
-        StringBuilder responseData = new();
-        int bytes = stream.Read(data, 0, data.Length);
-        responseData.Append(Encoding.ASCII.GetString(data, 0, bytes));
-        Console.WriteLine("Server response: " + responseData.ToString());
-
-        // Send data
-        Console.WriteLine("What to write back?");
-        message = Console.ReadLine() + "";
-        string json = JsonSerializer.Serialize(messageObject);
-        byte[] byteArray = Encoding.ASCII.GetBytes(json);
-        stream.Write(byteArray, 0, byteArray.Length);
-    }*/
-}
-catch (Exception e)
-{
-    Console.WriteLine("Error: {0}", e.Message);
+    }
 }
