fc6bd0a4e16e5af1fed0286e643d75c7e587849c
eskil4152 <eskiltb@hotmail.com>
2024-03-15 12:45:57 +0100

    structure changes and added server-client

---
+++ ChatApplicationServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var localAddress = "";
+int port = 8081;
+
+var host = Dns.GetHostEntry(Dns.GetHostName());
+foreach (var ip in host.AddressList)
+{
+    if (ip.AddressFamily == AddressFamily.InterNetwork)
+    {
+        System.Console.WriteLine(ip.ToString());
+        localAddress = ip.ToString();
+    }
+}
+
+TcpListener server = new(IPAddress.Parse(localAddress), port);
+server.Start();
+
+Console.WriteLine("Server started on {0}:{1}.\r\n Waiting for connection...\r\n", localAddress, port);
+
+TcpClient client = server.AcceptTcpClient();
+
+System.Console.WriteLine("Client connected\r\n");
+
+server.Stop();
---
+++ ChatApplicationServerClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var hostIp = "192.168.0.135";
+int hostPort = 8081;
+
+try
+{
+    TcpClient client = new(hostIp, hostPort);
+
+    Console.WriteLine("Client connected");
+}
+catch
+{
+    Console.WriteLine("Error");
+}
