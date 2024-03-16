7e1d0996f61392d8a8d8a8851021ccb6e43df096
eskil4152 <eskiltb@hotmail.com>
2024-03-16 15:43:59 +0100

    page checks if logged in and saves values from input

--- ChatApplicationClient/src/pages/LoginPage.js
+++ ChatApplicationClient/src/pages/LoginPage.js
@@ -1,10 +1,46 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function LoginPage() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("Username: " + username);
+    console.log("Pword: " + password);
+
+    setLoggedIn(!loggedIn);
+  }
+
   return (
     <div>
-      <h1>HELLo form lgin</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button>Confirm</button>
+      </form>
+
       <Link to={"/test"}>BUTTON</Link>
+
+      {loggedIn ? (
+        <div>
+          <p>Logged in</p>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
