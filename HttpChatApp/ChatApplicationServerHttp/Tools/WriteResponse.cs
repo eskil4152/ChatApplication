579e112255fb653a3cb6847822409499c25db97c
eskil4152 <eskiltb@hotmail.com>
2024-03-21 13:41:29 +0100

    Tools

---
+++ HttpChatApp/ChatApplicationServerHttp/Tools/WriteResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace ChatApplicationServerHttp
+{
+	public class WriteResponse
+	{
+        public static async Task WriteJsonResponse(HttpListenerContext context, object responseObject)
+        {
+            string jsonResponse = JsonSerializer.Serialize(responseObject);
+            byte[] responseBytes = Encoding.UTF8.GetBytes(jsonResponse);
+
+            context.Response.ContentType = "application/json";
+            context.Response.ContentEncoding = Encoding.UTF8;
+            context.Response.ContentLength64 = responseBytes.Length;
+
+            Cookie cookie = new("coo", "cook");
+            context.Response.Cookies.Add(cookie);
+
+            await context.Response.OutputStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+            context.Response.Close();
+        }
+    }
+}
+
