4cca2c3dac8202835ff4699e3f39b2e5c9b726fe
eskil4152 <eskiltb@hotmail.com>
2024-04-04 19:51:15 +0200

    rename server

--- ChatApplicationServer/Program.cs
+++ ChatApplicationServer/Program.cs
@@ -28,15 +28,6 @@ builder.Services.AddCors(options =>
                 .AllowCredentials()
                 .AllowAnyMethod();
         });
-
-    options.AddPolicy("AllowNgrok",
-            builder =>
-            {
-                builder.WithOrigins("https://89cf-195-139-192-113.ngrok-free.app")
-                       .AllowAnyHeader()
-                       .AllowCredentials()
-                       .AllowAnyMethod();
-            });
 });
 
 var app = builder.Build();
